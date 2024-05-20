8e773b81f07c936926828fee6408cdb6a34cdf7d
Arctic Fox <tyskanhik1@mail.ru>
2024-05-20 14:43:02 +0300

    Форма

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,7 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton onClick={() => console.log('123')} isOpen={false}/>
 			</>
 		);
 	},
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,60 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
+import { Text } from '../text';
+import { Select } from '../select';
+import { ArticleStateType, OptionType, backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+
+interface ParamsForms {
+	ArticleStateType: ArticleStateType;
+}
+
+
+export const ArticleParamsForm = (props: ParamsForms) => {
+	const [isOpen, setOpen] = useState(false)
+	const [state, setState] = useState<ArticleStateType>(props.ArticleStateType)
+
+	const openedForm = () => {
+		setOpen(!isOpen)
+	}
+
+	const changeFontFamily = (value: OptionType) => {
+		setState({ ...state, fontFamilyOption: value })
+	}
+
+	const changeFontSize = (value: OptionType) => {
+		setState({ ...state, fontSizeOption: value })
+	}
+
+	const changeFontColors = (value: OptionType) => {
+		setState({...state, fontColor: value})
+	}
+
+	const changeBackgroundColors = (value: OptionType) => {
+		setState({ ...state, backgroundColor: value })
+	}
+
+	const changeContentWidth = (value: OptionType) => {
+		setState({...state, contentWidth: value})
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={() => openedForm()} isOpen={isOpen} />
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text weight={800} uppercase={true} size={31}>Задайте параметры</Text>
+					<Select options={fontFamilyOptions} selected={state.fontFamilyOption} title='Шрифт' onChange={changeFontFamily}></Select>
+					<RadioGroup name='font size' options={fontSizeOptions} selected={state.fontSizeOption} title='Размер шрифта' onChange={changeFontSize}></RadioGroup>
+					<Select options={fontColors} selected={state.fontColor} title='Цвет шрифта' onChange={changeFontColors}></Select>
+					<hr/>
+					<Select options={backgroundColors} selected={state.backgroundColor} title='Цвет фона' onChange={changeBackgroundColors}></Select>
+					<Select options={contentWidthArr} selected={state.contentWidth} title='Ширина контента' onChange={changeContentWidth}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
@@ -18,3 +64,4 @@ export const ArticleParamsForm = () => {
 		</>
 	);
 };
+<aside ></aside>
